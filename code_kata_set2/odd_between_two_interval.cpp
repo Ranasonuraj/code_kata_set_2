254dc6fb861c5f977f79acefafecc7d668792fd6
Ranasonuraj <52693912+Ranasonuraj@users.noreply.github.com>
2019-08-06 03:17:07 +0530

    Update odd_between_two_interval.cpp

--- code_kata_set2/odd_between_two_interval.cpp
+++ code_kata_set2/odd_between_two_interval.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
 int main()
+    
 {
     int n,n1,n2,i;
     cin>>n1>>n2;
     for(i=n1;i<n2-1;i++)
+        
     {
          n=i+1;
         if(n%2!=0)
@@ -12,5 +14,6 @@ int main()
             cout <<n<<" ";
         }
         }
+    
     return 0;
 }
