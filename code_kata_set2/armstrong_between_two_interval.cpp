b866066988a1a2203d1a7a91bcce94be315aabbd
Ranasonuraj <52693912+Ranasonuraj@users.noreply.github.com>
2019-08-06 03:23:13 +0530

    Update armstrong_between_two_interval.cpp

--- code_kata_set2/armstrong_between_two_interval.cpp
+++ code_kata_set2/armstrong_between_two_interval.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
 int main()
+    
 {
     int n,n1,num,n2,r,sum=0;
     cin>>n1>>n2;
     for(int i=n1;i<n2-1;i++)
+    
     {
         n=i+1;
         num=n;
@@ -20,6 +22,6 @@ int main()
          }
          sum=0;
         }
-
     return 0;
+    
 }
